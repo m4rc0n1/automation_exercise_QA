5f26406296f4c7b5ddf4a28d56e4ebfedac721a9
ZizaAli <ziza.aliyyeva@gmail.com>
2025-06-20 17:16:13 +0200

    delete token

--- src/test/java/tests/api/githubTest/NegativeGithubTest.java
+++ src/test/java/tests/api/githubTest/NegativeGithubTest.java
@@ -1,24 +1,19 @@
 package tests.api.githubTest;
 
-import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
-import pages.api.models.RepoModel;
 import utils.ConfigReader;
-
-import static com.google.common.base.Predicates.equalTo;
 
 public class NegativeGithubTest {
     ConfigReader configReader = new ConfigReader();
-    Gson gson = new Gson();
     String PTA = configReader.getProperty("PTA");
     String ApiUrl = configReader.getProperty("ApiUrl");
     String githubUsername = configReader.getProperty("GithubUsername");
 
     @Test
-    public void deleteNotExisting() {
+    public void deleteRepoInvalidToken(){
         RestAssured.baseURI = ApiUrl;
         RestAssured
                 .given().auth().oauth2("invalid_token").contentType(ContentType.JSON)
@@ -27,7 +22,16 @@ public class NegativeGithubTest {
                 .then().statusCode(401)
                 .body("message", Matchers.equalTo("Bad credentials"));
     }
-
+    @Test
+    public void deleteNonExistingRepo(){
+        RestAssured.baseURI = ApiUrl;
+        RestAssured
+                .given().auth().oauth2(PTA).contentType(ContentType.JSON)
+                .when()
+                .delete("repos/Gizgayit/repoModel000")
+                .then().statusCode(404)
+                .body("message", Matchers.equalTo("Bad credentials"));
+    }
 }
 
 
