5942c2fa7992dd5b645db2eabfd76453331e0db7
Qizqayit <qizqayit@gmail.com>
2025-06-27 12:22:59 -0400

    changed

--- src/test/java/tests/unit/EvTapsirigi.java
+++ src/test/java/tests/unit/EvTapsirigi.java
@@ -1,16 +1,16 @@
-package tests.unit;
-
-public class EvTapsirigi {
-
-    public static void main(String[] args) {
-        String inform = personalMelumat("Ilyas", 45);
-        System.out.println(inform);
-
-    }
-
-    public static String personalMelumat(String insanAdi, int yas) {
-        return yas+" "+"yasli"+" "+insanAdi+" "+"Java oyrenir.";
-
-    }
-
-}
+//package tests.unit;
+//
+//public class EvTapsirigi {
+//
+//    public static void main(String[] args) {
+//        String inform = personalMelumat("Ilyas", 45);
+//        System.out.println(inform);
+//
+//    }
+//
+//    public static String personalMelumat(String insanAdi, int yas) {
+//        return yas+" "+"yasli"+" "+insanAdi+" "+"Java oyrenir.";
+//
+//    }
+//
+//}
